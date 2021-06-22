149db942651c8b4c5db72fee672c90e92f94c597
TarifSadman228 <sadmantaha9991@gmail.com>
2021-06-22 16:00:03 +0600

    started project works

---
+++ LAB4/Task.php
@@ -0,0 +1,234 @@
+<?php
+	$name="";
+	$err_name="";
+	$username="";
+	$err_username="";
+	$password="";
+	$err_password="";
+	$confirmpassword="";
+	$err_confirmpassword="";
+	$email="";
+	$err_email="";
+	$phone="";
+	$err_phone="";
+	$address="";
+	$birthdate="";
+	$err_birthdate="";
+	$gender="";
+	$err_gender="";
+	$heardfrom="";
+	$err_heardfrom="";
+	$bio="";
+	$err_bio="";
+	
+	$hasError=false;
+	
+	            if($_SERVER['REQUEST_METHOD']== "POST")
+				{
+                if(empty($_POST["name"]))
+				{
+				$hasError = true;
+                $err_name="No name added";
+                }
+                else
+				{
+				$name=$_POST["name"];
+				}
+				if(empty($_POST["username"]))
+				{
+				$hasError = true;
+				$err_username="No username added";
+				}
+				else if(strlen($_POST["username"])<6)
+				{
+				$hasError = true;
+				$err_username="*Username must be of => 6 characters";
+				}
+                else if (strpos($_POST["username"]," "))
+				{
+				$hasError = true;
+                $err_username="Invalid space used";
+                }
+				else
+				{	
+				$username=$_POST["username"];
+				}
+				if(empty($_POST["password"]))
+				{
+				$hasError = true;
+				$err_username="No password added";
+				}
+				else if(htmlspecialchars($_POST["password"]))
+		 		{
+				$hasError = true;
+				$err_password="Invalid HTML special characters used";
+				}
+		 		else if(strlen($_POST["password"])<8) 
+				{
+				$hasError = true;
+		 		$err_password="Password must contain =>8 characters";
+		 		}
+		 		else if(!strpos($_POST["password"],"#&&?"))
+		 		{	
+			    $err_password="Password must contain at least a special character";
+			    }
+				else if(!is_numeric($_POST["password"]))
+				{
+				$err_password="Password must contain =>1 numeric values";
+				}
+				else if(!ctype_upper($_POST["Password"]))
+				{
+				$err_password="Password must contain =>1 upper case values";
+				}
+				else if(!ctype_lower($_POST["password"]))
+				{
+				$err_password="Password must contain =>1 lower case values";
+                }
+				else
+				{
+				$password=$_POST["password"];
+				}
+				if(empty($_POST["confirmpassword"]))
+				{
+				$hasError = true;
+				$err_password="Retype password";
+				}
+				else if(strcmp ($_POST["password"],$_POST["confirmpassword"])==false)
+				$hasError = true;
+			    $err_confirmpassword="Passwords aren't matched"
+				else
+				{
+				$confirmpassword="$_POST["confirmpassword"]
+				}
+	
+	//if($_SERVER["REQUEST_METHOD"]=="POST"){
+	if(isset($_POST["submit"])){
+		if(empty($_POST["name"])){
+			$hasError = true;
+			$err_name="Name Required";
+		}
+		else if(strlen($_POST["name"]) <= 2){
+			$hasError = true;
+			$err_name="Name must contain >2 character";
+		}
+		else{
+			$name = $_POST["name"];
+		}
+		if(!isset($_POST["gender"])){
+			$hasError = true;
+			$err_gender="Gender Required";
+		}
+		else{
+			$gender = $_POST["gender"];
+		}
+		if(!isset($_POST["hobbies"])){
+			$hasError = true;
+			$err_hobbies="Hobbies Required";
+		}
+		else{
+			$hobbies = $_POST["hobbies"];
+		}
+		if (!isset($_POST["profession"])){
+			$hasError = true;
+			$err_profession="Profession Required";
+		}
+		else{
+			$profession = $_POST["profession"];
+		}
+		if(empty($_POST["bio"])){
+			$hasError = true;
+			$err_bio = "Bio Required";
+		}
+		else{
+			$bio = $_POST["bio"];
+		}
+		
+		
+		if(!$hasError){
+			echo "<h1>Form submitted</h1>";
+			echo $_POST["name"]."<br>";
+			echo $_POST["username"]."<br>";
+			echo $_POST["password"]."<br>";
+			echo $_POST["gender"]."<br>";
+			echo $_POST["profession"]."<br>";
+			echo $_POST["bio"]."<br>";
+			$arr = $_POST["hobbies"];
+
+			foreach($arr as $e){
+				echo "$e<br>";
+			}
+		}
+		
+		//we will otherwise DB CRUD or authenticate
+		///header("Location: index.php");
+	}
+	
+?>
+<html>
+	<head></head>
+	<body>
+		<form action="" method="post">
+		<fieldset>
+			<table>
+				<tr>
+					<td>Name</td>
+					<td>: <input type="text" name="name" value="<?php echo $name; ?>" placeholder="Your name ...."> </td>
+					<td><span> <?php echo $err_name;?> </span></td>
+				</tr>
+				<tr>
+					<td>Username</td>
+					<td>: <input type="text" name="username" placeholder="Username">  </td>
+					<td><span> <?php echo $err_username;?> </span></td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td>: <input type="password" name="password" placeholder="Password">  </td>
+					<td><span> <?php echo $err_password;?> </span></td>
+				</tr>
+				<tr>
+					<td>Gender</td>
+					<td>: <input type="radio" value="Male" <?php if($gender=="Male") echo "checked"; ?> name="gender"> Male <input name="gender" <?php if($gender=="Female") echo "checked"; ?> value="Female" type="radio"> Female </td>
+					<td><span> <?php echo $err_gender;?> </span></td>
+				</tr>
+				<tr>
+					<td>Profession</td>
+					<td>: <select name="profession">
+						<option disabled selected>---Select---</option>
+						<?php
+							foreach($array as $p){
+								if($p == $profession) 
+									echo "<option selected>$p</option>";
+								else
+									echo "<option>$p</option>";
+							}
+						?>
+						</select>
+					</td>
+					<td><span> <?php echo $err_profession;?> </span></td>
+				</tr>
+				<tr>
+					<td>Hobbies</td>
+					<td>: <input type="checkbox" name="hobbies[]" <?php if(hobbyExist("Movies")) echo "checked";?> value="Movies"> Movies 
+					<input type="checkbox" name="hobbies[]" <?php if(hobbyExist("Music")) echo "checked";?> value="Music"> Music<br>
+					<input type="checkbox" name="hobbies[]" <?php if(hobbyExist("Sports")) echo "checked";?> value="Sports"> Sports
+					</td>
+					<td><span> <?php echo $err_hobbies;?> </span></td>
+				</tr>
+				<tr>
+					<td>Bio</td>
+					<td>: <textarea name="bio" ><?php echo $bio; ?></textarea>
+					<td><span> <?php echo $err_bio;?> </span></td>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2" align="right"><input type="submit" name="submit" value="Submit"></td>
+					
+				</tr>
+			</table>
+			
+			
+			
+		</fieldset>
+		</form>
+	</body>
+</html>
